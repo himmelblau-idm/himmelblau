71fba60ba5019cc7973ddf576e9c8d38a0f7662b
David Mulder <dmulder@samba.org>
2023-06-30 14:54:59 -0600

    tests: Test pam auth
    This creates a build environment in a podman
    container, then executes the tests.
    
    Signed-off-by: David Mulder <dmulder@samba.org>

---
+++ tests/runner.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import unittest
+import pamtest
+import signal
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--hang", action="store_true",
+                        help="Intentionally hang the program to allow exec'ing the container")
+    args = parser.parse_args()
+
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+
+    suite.addTests(loader.loadTestsFromModule(pamtest))
+
+    runner = unittest.TextTestRunner(verbosity=3, failfast=True)
+    runner.run(suite)
+
+    if args.hang:
+        while True:
+            pass
