54bc6ff094e0ff7f2c08a364510137916678e564
David Mulder <dmulder@samba.org>
2023-07-31 09:48:03 -0600

    test: Separate project build from docker build
    This speeds up the build for tests.
    
    Signed-off-by: David Mulder <dmulder@samba.org>

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Build himmelblau
+mkdir /root/tests/build
+pushd /root/tests/build
+tar -xf /root/tests/himmelblau.tar.gz && cargo build
+install -m 0755 ./target/debug/libnss_himmelblau.so /usr/lib64/libnss_himmelblau.so.2
+ln -s /usr/lib64/libnss_himmelblau.so.2 /usr/lib64/libnss_himmelblau.so
+install -m 0755 ./target/debug/libpam_himmelblau.so /usr/lib64/security/pam_himmelblau.so
+install -m 0755 ./target/debug/himmelblaud /usr/sbin
+install -m 0755 ./target/debug/aad-tool /usr/bin
+popd
+
 # Build the test configuration
 /root/tests/buildconfig.py
 
