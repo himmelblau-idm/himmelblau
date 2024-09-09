6c5cfefc4939897153af2d50e682a50829021d0d
David Mulder <dmulder@samba.org>
2024-09-09 14:59:13 -0600

    Fix clippy warnings
    Signed-off-by: David Mulder <dmulder@samba.org>

--- src/crypto/src/lib.rs
+++ src/crypto/src/lib.rs
@@ -353,13 +353,7 @@ impl CryptoPolicy {
                             .unwrap_or(ARGON2_MIN_RAM_KIB);
 
                         // Floor and Ceil
-                        m_cost = if m_adjust > ARGON2_MAX_RAM_KIB {
-                            ARGON2_MAX_RAM_KIB
-                        } else if m_adjust < ARGON2_MIN_RAM_KIB {
-                            ARGON2_MIN_RAM_KIB
-                        } else {
-                            m_adjust
-                        };
+                        m_cost = m_adjust.clamp(ARGON2_MIN_RAM_KIB, ARGON2_MAX_RAM_KIB);
                         continue;
                     } else {
                         // Unable to proceed, parameters are maxed out.
