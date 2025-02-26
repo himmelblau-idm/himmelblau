bcacfc67e282172f397e4d2b35b0a1a2c13a3e22
David Mulder <dmulder@samba.org>
2025-02-26 13:14:22 -0700

    Default to request group info via Edge browser
    The MS Edge browser app_id grants us the ability
    to read group names.
    
    Signed-off-by: David Mulder <dmulder@samba.org>

--- src/common/src/constants.rs
+++ src/common/src/constants.rs
@@ -47,3 +47,4 @@ pub const BROKER_CLIENT_IDENT: &str = "38aa3b87-a06d-4817-b275-7a316988d93b";
 pub const CN_NAME_MAPPING: bool = true;
 pub const DEFAULT_HELLO_PIN_MIN_LEN: usize = 6;
 pub const DEFAULT_CCACHE_DIR: &str = "/tmp/krb5cc_";
+pub const EDGE_BROWSER_CLIENT_ID: &str = "d7b530a4-7680-4c23-a8bf-c52c121d2e87";
