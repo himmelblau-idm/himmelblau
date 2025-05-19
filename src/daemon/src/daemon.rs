5bb3134af87067f087d26922212b33cf04f7cb2e
David Mulder <dmulder@samba.org>
2025-05-19 02:24:42 -0600

    systemd notify fixes
    Signed-off-by: David Mulder <dmulder@samba.org>

--- src/daemon/src/daemon.rs
+++ src/daemon/src/daemon.rs
@@ -1288,7 +1288,9 @@ async fn main() -> ExitCode {
             info!("Server started ...");
 
             if systemd_booted {
-                let _ = sd_notify::notify(true, &[NotifyState::Ready]);
+                if let Ok(monotonic_usec) = sd_notify::NotifyState::monotonic_usec_now() {
+                    let _ = sd_notify::notify(true, &[NotifyState::Ready, monotonic_usec]);
+                }
             }
 
             drop(_enter);
@@ -1338,7 +1340,9 @@ async fn main() -> ExitCode {
 
             info!("Signal received, sending down signal to tasks");
             if systemd_booted {
-                let _ = sd_notify::notify(true, &[NotifyState::Stopping]);
+                if let Ok(monotonic_usec) = sd_notify::NotifyState::monotonic_usec_now() {
+                    let _ = sd_notify::notify(true, &[NotifyState::Stopping, monotonic_usec]);
+                }
             }
 
             // Send a broadcast that we are done.
