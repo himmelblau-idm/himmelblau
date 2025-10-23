26974e811c1caeed62d276b4aa163ebd3cac377f
David Mulder <dmulder@samba.org>
2025-10-23 08:53:03 -0600

    Distinguish between online and offline token fail
    Signed-off-by: David Mulder <dmulder@samba.org>

--- src/common/src/resolver.rs
+++ src/common/src/resolver.rs
@@ -1238,9 +1238,16 @@ where
                     }
                 }
             }
-            (&mut AuthSession::InProgress { token: None, .. }, _) => {
-                // Can't do much with offline auth when there is no token ...
-                warn!("Unable to proceed with offline auth, no token available");
+            (&mut AuthSession::InProgress { token: None, .. }, state) => {
+                // Can't do much with online/offline auth when there is no token ...
+                match state {
+                    CacheState::Online => {
+                        warn!("Unable to proceed with online auth, no token available");
+                    }
+                    CacheState::Offline | CacheState::OfflineNextCheck(_) => {
+                        warn!("Unable to proceed with offline auth, no token available");
+                    }
+                }
                 Err(IdpError::NotFound { what: "token".to_string(), where_: "AuthSession".to_string() })
             }
             (&mut AuthSession::Success(_), _) | (&mut AuthSession::Denied, _) => {
