fce6270a2b84d98ea019e479a605192e341977b3
David Mulder <dmulder@samba.org>
2025-11-26 12:01:59 -0700

    Generate a RPM spec file for SUSE Linux
    Signed-off-by: David Mulder <dmulder@samba.org>

--- scripts/gen_dockerfiles.py
+++ scripts/gen_dockerfiles.py
@@ -37,7 +37,7 @@ COMMON = [
     "checkpolicy",
     "policycoreutils",
     "python3",
-    "systemd",
+    "systemd-devel",
 ]
 
 PKG_PAIRS = [
@@ -305,6 +305,7 @@ DISTS = {
             "dbus-devel": "dbus-1-devel",
             "tpm2-tss-devel": "tpm2-0-tss-devel",
             "sqlite-devel": "sqlite3-devel",
+            "selinux-policy-targeted": "selinux-tools selinux-policy-devel",
         },
         "tpm": True,
         "selinux": True,
