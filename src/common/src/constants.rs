99050ac8ec4dc1496c829aa2eba25035341417ef
David Mulder <dmulder@samba.org>
2026-01-07 15:35:04 -0700

    Disable Hello TOTP by default
    Signed-off-by: David Mulder <dmulder@samba.org>

--- src/common/src/constants.rs
+++ src/common/src/constants.rs
@@ -66,4 +66,4 @@ pub const CONFIDENTIAL_CLIENT_SECRET_TAG: &str = "confidential_client_secret";
 pub const CONFIDENTIAL_CLIENT_CERT_TAG: &str = "confidential_client_certificate";
 pub const DEFAULT_JOIN_TYPE: JoinType = JoinType::Join;
 pub const DEFAULT_OFFLINE_BREAKGLASS_TTL: u64 = 7200;
-pub const DEFAULT_HELLO_TOTP_ENABLED: bool = true;
+pub const DEFAULT_HELLO_TOTP_ENABLED: bool = false;
