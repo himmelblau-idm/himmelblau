bcd2d95833faeb3a978a173032c1fdc7b961f2ba
Bash Bandicoot <bash-bandicoot@users.noreply.github.com>
2026-02-19 12:46:40 -0800

    resolver: add change_auth_token_local() dispatcher

--- src/common/src/resolver.rs
+++ src/common/src/resolver.rs
@@ -767,6 +767,40 @@ where
         })
     }
 
+    pub async fn change_auth_token_local(
+        &self,
+        account_id: &str,
+        old_pin: &str,
+        new_pin: &str,
+    ) -> Result<bool, ()> {
+        // Validate the user isn't in the nxset (aka, it's a local user or group).
+        if self.check_nxset(Some(account_id), None).await {
+            return Ok(false);
+        }
+
+        let mut hsm_lock = self.hsm.lock().await;
+        let mut dbtxn = self.db.write().await;
+
+        let res = self
+            .client
+            .change_auth_token_local(
+                account_id,
+                old_pin,
+                new_pin,
+                &mut dbtxn,
+                hsm_lock.deref_mut(),
+                &self.machine_key,
+            )
+            .await;
+
+        drop(hsm_lock);
+        dbtxn.commit().map_err(|_| ())?;
+
+        res.map_err(|e| {
+            trace!("change_auth_token_local error -> {:?}", e);
+        })
+    }
+
     pub async fn offline_break_glass(&self, ttl: Option<u64>) -> Result<(), ()> {
         let res = self.client.offline_break_glass(ttl).await;
 
