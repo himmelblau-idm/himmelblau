55171bff5ee4a5299612655c2f6ba1b6a8b415e4
Luca Boccassi <luca.boccassi@gmail.com>
2026-03-03 11:03:40 +0000

    broker: use SSO server nonce for PRT only when provided
    With the new libhimmelblau we can request an issued-at-time
    token, which ensures a longer validity. linux-entra-sso uses
    the token for 30 minutes, but currently it expires after 5/10,
    causing interruptions for users.
    Use the server sso nonce only if provided.
    
    Fixes https://github.com/himmelblau-idm/himmelblau/issues/1164
    
    Signed-off-by: Luca Boccassi <luca.boccassi@gmail.com>

--- src/common/src/idprovider/interface.rs
+++ src/common/src/idprovider/interface.rs
@@ -278,6 +278,7 @@ pub trait IdProvider {
         &self,
         _id: &Id,
         _token: Option<&UserToken>,
+        _sso_nonce: Option<&str>,
         _keystore: &mut D,
         _tpm: &mut tpm::provider::BoxedDynTpm,
         _machine_key: &tpm::structures::StorageKey,
