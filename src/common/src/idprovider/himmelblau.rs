0c7c7522b06e8478b4305b784b43da307d0830b7
Luca Boccassi <luca.boccassi@gmail.com>
2026-03-03 11:03:40 +0000

    broker: use SSO server nonce for PRT only when provided
    (cherry-picked from 570ed3ce34e77be3d1ec33f27e0e58e220c5e9a9)
    
    Signed-off-by: Luca Boccassi <luca.boccassi@gmail.com>

--- src/common/src/idprovider/himmelblau.rs
+++ src/common/src/idprovider/himmelblau.rs
@@ -443,6 +443,7 @@ impl IdProvider for HimmelblauMultiProvider {
         &self,
         id: &Id,
         old_token: Option<&UserToken>,
+        sso_nonce: Option<&str>,
         tpm: &mut tpm::provider::BoxedDynTpm,
         machine_key: &tpm::structures::StorageKey,
     ) -> Result<String, IdpError> {
@@ -455,7 +456,7 @@ impl IdProvider for HimmelblauMultiProvider {
         let provider = find_provider!(self, providers, domain)?;
 
         provider
-            .unix_user_prt_cookie(id, old_token, tpm, machine_key)
+            .unix_user_prt_cookie(id, old_token, sso_nonce, tpm, machine_key)
             .await
     }
 
@@ -929,6 +930,7 @@ impl IdProvider for HimmelblauProvider {
         &self,
         id: &Id,
         old_token: Option<&UserToken>,
+        sso_nonce: Option<&str>,
         tpm: &mut tpm::provider::BoxedDynTpm,
         machine_key: &tpm::structures::StorageKey,
     ) -> Result<String, IdpError> {
@@ -949,7 +951,7 @@ impl IdProvider for HimmelblauProvider {
         self.client
             .read()
             .await
-            .acquire_prt_sso_cookie(&prt, tpm, machine_key)
+            .acquire_prt_sso_cookie_with_nonce(&prt, sso_nonce, tpm, machine_key)
             .await
             .map_err(|e| {
                 error!("Failed to request prt cookie: {:?}", e);
